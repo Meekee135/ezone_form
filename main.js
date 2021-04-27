3f878ae46917c29183e0718c11a700ab539fe112
Marko Zabrdac <mark287c@stud.kea.dk>
2021-04-27 15:44:08 +0200

    articles finished minus small styling

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ import "./styles/main.scss";
 
 import { gsap, ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
+
+
+document.querySelector(".a3 .btn").addEventListener("click", openModal);
+
+function openModal() {
+    document.querySelector(".modal-article").style.display = "block";
+
+    document.querySelector(".closeArticle").addEventListener("click", () => {
+        document.querySelector(".modal-article").style.display = "none";
+    })
+}
