08fc0ccaa76686793ddf27f563d07df965f2ab63
Miko <kozak.mikolaj@gmail.com>
2021-04-27 16:50:51 +0200

    cursor add form fixes

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 import "./styles/main.scss";
+import curDot from "cursor-dot";
+// const cursor = curDot();
+// cursor({
+//   //   zIndex: 2,
+//   diameter: 80,
+//   borderWidth: 1,
+//   //   borderColor: "transparent",
+//   //   easing: 4,
+//   //   background: "#ddd",
+// });
 
 import { gsap, ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
-
 document.querySelector(".a3 .btn").addEventListener("click", openModal);
 
 function openModal() {
-    document.querySelector(".modal-article").style.display = "block";
+  document.querySelector(".modal-article").style.display = "block";
 
-    document.querySelector(".closeArticle").addEventListener("click", () => {
-        document.querySelector(".modal-article").style.display = "none";
-    })
+  document.querySelector(".closeArticle").addEventListener("click", () => {
+    document.querySelector(".modal-article").style.display = "none";
+  });
 }
